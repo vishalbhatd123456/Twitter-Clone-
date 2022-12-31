b73f2daf49fc2763f13ad95ebe655d664457a3b2
vishalbhatd123456 <vishaldbhat123456@gmail.com>
2022-12-31 10:45:24 +0530

    rever switch case for routing and added nodemon dep

--- route.js
+++ route.js
@@ -1,22 +1,37 @@
+const { copyFile } = require("fs");
+
 const displayContent = (req, res) => {
 
     const url = req.url;
+    const method = req.method;
 
-    switch(url){
-        case "/profile":
-            res.setHeader("Content-Type","text/html");
+    if(url == "/profile"){
+        res.setHeader("Content-Type","text/html");
             res.write("This is a profile page!");
             return res.end();
-        case "/":
-            res.setHeader("Content-Type","text/html");
-            res.write("This is a default page");
+    }else if(url == "/"){
+        res.setHeader("Content-Type","text/html");
+            res.write("This is a default page!");
             return res.end();
-        default:
+    }
+    else if(url == "/settings" && method == "POST"){
+        res.setHeader("Content-Type","text/html");
+        res.write('Form was submitted');
+        return res.end();
+    }
+    else if(url == "/settings"){
+        res.setHeader("Content-Type","text/html");
+        res.write('<h1>Settings</h1><form action = "/settings" method = "POST"><input type = "text"><button type = "submit">Press Me!</button></form>');
+        return res.end();
+    }
+    else {
             res.setHeader("Content-Type","text/html");
-            res.write("404 Not Found");
+            res.write('404 Not Found');
             return res.end();
     }
 
+    
+    
 }
 
 module.exports = displayContent;
