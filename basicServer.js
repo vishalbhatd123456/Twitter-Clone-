1fff0f104ed8a8339c04db25bd4f174c907fcfcd
vishalbhatd123456 <vishaldbhat123456@gmail.com>
2022-12-31 09:58:45 +0530

    Added paging code

--- basicServer.js
+++ basicServer.js
@@ -1,8 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
+
+    const url = req.url;
+    if(url == "/profile"){
+        //this will be written over the server page
+        res.setHeader("Content-Type","text/html");
+        res.write("Art of living Profile Page!!");
+        return res.end();
+    }else if(url == "/home"){
+        res.setHeader("Content-Type","text/html");
+        res.write("Art Of Living Home Page");
+        return res.end();
+    }
+
     //this will be written over the server page
-    res.write("Art of living");
+    res.setHeader("Content-Type","text/html");
+    res.write("Page Not Found 404!!");
     res.end();
 });
 
